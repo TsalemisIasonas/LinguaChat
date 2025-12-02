77d84dbf034b1a5bde79f6e603554fb22a5f2cd0
ntua-el21820 <el21820@mail.ntua.gr>
2025-12-02 14:24:39 +0200

    feat: add background gradient constant

--- lib/styles/colors.dart
+++ lib/styles/colors.dart
@@ -2,5 +2,15 @@ import 'package:flutter/material.dart';
 
 const Color backgroundColor = Color(0xFFF5F5F5);
 const Color navbarColor = Color(0xFF5C5891);
+
 const Color gradientColorStart = Color(0xFF7686EB);
 const Color gradientColorEnd = Color(0xFFA060BB);
+
+const BoxDecoration backgroundGradient = BoxDecoration(
+  gradient: LinearGradient(
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+    colors: [gradientColorStart, gradientColorEnd],
+    stops: [0, 0.9],
+  ),
+);
