6222e4609843eb7243e70def3d749aad905215fe
ntua-el21820 <el21820@mail.ntua.gr>
2025-12-03 17:22:09 +0200

    feat: ui settings screen and settings to update user data

---
+++ lib/screens/settings_screen.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+import 'package:lingua_chat/constants/types.dart';
+import 'package:lingua_chat/styles/colors.dart';
+import 'package:lingua_chat/models/database.dart';
+import 'package:lingua_chat/models/user.dart';
+import 'package:lingua_chat/widgets/app_bar.dart';
+import 'package:lingua_chat/widgets/language_dropdown.dart';
+import 'package:lingua_chat/widgets/user_level_button.dart';
+import 'package:lingua_chat/widgets/logout_button.dart';
+
+class SettingsScreen extends StatefulWidget {
+  static route() =>
+      MaterialPageRoute(builder: (context) => const SettingsScreen());
+
+  const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  Languages? _selectedLanguage = currentUser.language;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: backgroundGradient,
+      child: Scaffold(
+        extendBodyBehindAppBar: true,
+        backgroundColor: Colors.transparent,
+
+        appBar: linguaAppBar(title_: "Settings"),
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/app_logo.png'),
+              const SizedBox(height: 100),
+
+              languageDropdown(
+                value: _selectedLanguage,
+                onChanged: (valueChosen) {
+                  setState(() {
+                    _selectedLanguage = valueChosen;
+
+                    if (valueChosen == null) {
+                      return;
+                    }
+
+                    currentUser.language = valueChosen;
+                    DatabaseService().addOrUpdateUser(currentUser);
+                  });
+                },
+              ),
+              const SizedBox(height: 100),
+
+              levelButton(
+                label: UserLevel.beginner,
+                selected: currentUser.level,
+                onTap: () {
+                  setState(() {
+                    currentUser.level = UserLevel.beginner;
+                    DatabaseService().addOrUpdateUser(currentUser);
+                  });
+                },
+              ),
+              const SizedBox(height: 12),
+
+              levelButton(
+                label: UserLevel.intermediate,
+                selected: currentUser.level,
+                onTap: () {
+                  setState(() {
+                    currentUser.level = UserLevel.intermediate;
+                    DatabaseService().addOrUpdateUser(currentUser);
+                  });
+                },
+              ),
+              const SizedBox(height: 12),
+
+              levelButton(
+                label: UserLevel.advanced,
+                selected: currentUser.level,
+                onTap: () {
+                  setState(() {
+                    currentUser.level = UserLevel.advanced;
+                    DatabaseService().addOrUpdateUser(currentUser);
+                  });
+                },
+              ),
+              const SizedBox(height: 30),
+
+              logoutButton(context),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
