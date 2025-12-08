d08f498a493815c8c2999b2d00ccb0b3f891d677
ntua-el21820 <el21820@mail.ntua.gr>
2025-12-08 18:05:32 +0200

    feat: add leaderboard design and functionality

--- lib/screens/leaderboard_screen.dart
+++ lib/screens/leaderboard_screen.dart
@@ -1,57 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:lingua_chat/screens/home_screen.dart';
+
 import 'package:lingua_chat/styles/colors.dart';
+import 'package:lingua_chat/widgets/app_bar.dart';
 import 'package:lingua_chat/widgets/floating_action_button.dart';
 import 'package:lingua_chat/widgets/navigation_bar.dart';
+import 'package:lingua_chat/widgets/leaderboard.dart';
 
 class LeaderboardScreen extends StatelessWidget {
   const LeaderboardScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      backgroundColor: backgroundColor,
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: SizedBox(
-        width: 80,
-        height: 80,
-        child: LinguaFloatingActionButton(),
-      ),
-
-      appBar: AppBar(
-        centerTitle: true,
+    return Container(
+      decoration: backgroundGradient,
+      child: Scaffold(
+        extendBodyBehindAppBar: true,
         backgroundColor: Colors.transparent,
-        elevation: 0,
-        title: const Text('Top Learners in your region'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.home),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const HomeScreen()),
-              );
-            },
-          ),
-        ],
-      ),
-
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              gradientColorStart,
-              gradientColorEnd,
-            ],
-            stops: [0.1, 0.3],
-          ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: SizedBox(
+          width: 80,
+          height: 80,
+          child: LinguaFloatingActionButton(),
         ),
-      ),
 
-      bottomNavigationBar: const LinguaNavigationBar(),
+        appBar: linguaAppBar(title_: 'Leaderboard'),
+
+        body: LeaderboardWidget(),
+
+        bottomNavigationBar: const LinguaNavigationBar(),
+      ),
     );
   }
 }
