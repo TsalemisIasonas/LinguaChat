583fee9ca287b16b7f62fdf72aff9f85d049836a
TsalemisIasonas <tsalemisi@gmail.com>
2026-01-05 18:57:45 +0200

    Functional_UI

---
+++ lib/config/api_keys.dart
@@ -0,0 +1,2 @@
+// API Keys - DO NOT commit this file to version control
+const String openAiApiKey = "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";
